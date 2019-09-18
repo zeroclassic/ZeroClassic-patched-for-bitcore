9d1f57e21524fc6820a77901f1d5712f531762e6
miodragpop <miodragpopovic@stcable.net>
2019-09-18 17:39:35 +0200

    added networksolps property to getinfo RPC call

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -41,6 +41,9 @@ using namespace std;
  *
  * Or alternatively, create a specific query method for the information.
  **/
+
+extern int64_t GetNetworkHashPS(int lookup, int height); 
+ 
 UniValue getinfo(const UniValue& params, bool fHelp)
 {
     if (fHelp || params.size() != 0)
@@ -58,6 +61,7 @@ UniValue getinfo(const UniValue& params, bool fHelp)
             "  \"connections\": xxxxx,       (numeric) the number of connections\n"
             "  \"proxy\": \"host:port\",     (string, optional) the proxy used by the server\n"
             "  \"difficulty\": xxxxxx,       (numeric) the current difficulty\n"
+            "  \"networksolps\": x           (numeric) The estimated network solution rate in Sol/s\n"
             "  \"testnet\": true|false,      (boolean) if the server is using testnet or not\n"
             "  \"keypoololdest\": xxxxxx,    (numeric) the timestamp (seconds since GMT epoch) of the oldest pre-generated key in the key pool\n"
             "  \"keypoolsize\": xxxx,        (numeric) how many new keys are pre-generated\n"
@@ -94,6 +98,7 @@ UniValue getinfo(const UniValue& params, bool fHelp)
     obj.push_back(Pair("connections",   (int)vNodes.size()));
     obj.push_back(Pair("proxy",         (proxy.IsValid() ? proxy.proxy.ToStringIPPort() : string())));
     obj.push_back(Pair("difficulty",    (double)GetDifficulty()));
+    obj.push_back(Pair("networksolps",  GetNetworkHashPS(120, -1)));
     obj.push_back(Pair("testnet",       Params().TestnetToBeDeprecatedFieldRPC()));
 #ifdef ENABLE_WALLET
     if (pwalletMain) {
